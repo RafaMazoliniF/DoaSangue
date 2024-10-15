2de24759bde2c5812193dff130494fc3937f7472
Eduardo de Faria Rios Perucello <115468808+EduardoPerucello@users.noreply.github.com>
2024-10-14 22:27:40 -0300

    tela de login

--- cadastro/models.py
+++ cadastro/models.py
@@ -4,6 +4,7 @@ class Cadastro(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     idade = models.IntegerField()
+    senha = models.CharField(max_length=128, null=True)
 
     def __str__(self):
         return self.nome

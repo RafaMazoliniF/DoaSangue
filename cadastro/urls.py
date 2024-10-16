e2e627a2e3e88fb2ee4413c2b9015dd8bcd4e4f5
Yamakasuki <111364032+Yamakasuki@users.noreply.github.com>
2024-10-15 21:34:02 -0300

    Criação da minha Branch

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('new/', views.cadastro_create, name='cadastro_create'),
     path('<int:pk>/edit/', views.cadastro_update, name='cadastro_update'),
     path('<int:pk>/delete/', views.cadastro_delete, name='cadastro_delete'),
-    path('login', views.login_view, name='login_submit'),
+    path('login/', views.login_view, name='login_submit'),
     path('perfil/', user_profile_view, name='user_profile'),
-    path('logout/', logout_view, name='logout2'),
+    path('logout/', logout_view, name='logout'),
 ]
